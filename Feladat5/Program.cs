cbbfc8979e4c40946edc62bdb2c2cb6037d3f8d8
Macsoo <45904652+Macsoo@users.noreply.github.com>
2021-02-05 16:36:40 +0100

    Add files via upload

---
+++ Feladat5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Feladat5
+{
+    class Program
+    {
+        static int N;
+        static int[] jobs;
+        static List<int> firstMachine;
+        static List<int> secondMachine;
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            jobs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            firstMachine = new List<int>();
+            secondMachine = new List<int>();
+            firstMachine.Add(0);
+            FillMachines(ref firstMachine, ref secondMachine, 1);
+            Console.WriteLine(Math.Max(SumOfMachine(firstMachine), SumOfMachine(secondMachine)));
+            Console.WriteLine(string.Join(" ", firstMachine.Select(x => x + 1)));
+            Console.WriteLine(string.Join(" ", secondMachine.Select(x => x + 1)));
+        }
+        static int FillMachines(ref List<int> firstMachine, ref List<int> secondMachine, int index)
+        {
+            if(index == N)
+            {
+                return Math.Max(SumOfMachine(firstMachine), SumOfMachine(secondMachine));
+            }
+            List<int> tempFirst = new List<int>(firstMachine);
+            List<int> tempSecond = new List<int>(secondMachine);
+            tempFirst.Add(index);
+            tempSecond.Add(index);
+            int firstResult = FillMachines(ref tempFirst, ref secondMachine, index + 1);
+            int secondResult = FillMachines(ref firstMachine, ref tempSecond, index + 1);
+            if (firstResult < secondResult)
+            {
+                firstMachine = tempFirst;
+            }
+            else
+            {
+                secondMachine = tempSecond;
+            }
+            return Math.Max(SumOfMachine(firstMachine), SumOfMachine(secondMachine));
+        }
+        static int SumOfMachine(List<int> machine)
+        {
+            return machine.Sum(x => jobs[x]);
+        }
+    }
+}
---
+++ HanoiTornya/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace HanoiTornya
+{
+    class Program
+    {
+        static int height;
+        static StringBuilder sb;
+        static void Main(string[] args)
+        {
+            height = int.Parse(Console.ReadLine());
+            sb = new StringBuilder();
+            Move(1, 3, height);
+            Console.WriteLine(sb.ToString());
+        }
+        static void Move(int from, int to, int howMany)
+        {
+            if(howMany == 1)
+            {
+                sb.Append(from);
+                sb.Append('-');
+                sb.Append(to);
+                sb.Append('\n');
+                return;
+            }
+            int helper = 6 - from - to;
+            Move(from, helper, howMany - 1);
+            Move(from, to, 1);
+            Move(helper, to, howMany - 1);
+        }
+    }
+}
